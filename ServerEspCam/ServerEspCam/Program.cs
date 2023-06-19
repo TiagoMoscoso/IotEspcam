76e8bf6e35271815e07c82c4d534f5c67e1a484f
TiagoMoscoso <79027488+TiagoMoscoso@users.noreply.github.com>
2023-06-19 10:37:50 -0300

    LibRMV

--- ServerEspCam/ServerEspCam/Program.cs
+++ ServerEspCam/ServerEspCam/Program.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Net.Mail;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 using Npgsql;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
-using SendGrid;
-using SendGrid.Helpers.Mail;
+
 
 
 namespace ServerEspCam
